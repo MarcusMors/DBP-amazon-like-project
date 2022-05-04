0de9236f19bfb657434365dfa0cc0985bba75c09
Jose Vilca Campana @marcusmors <alivezeh@gmail.com>
2022-05-03 19:03:45 -0500

    :construction: key iteration and trying the script

--- main.py
+++ main.py
@@ -13,16 +13,25 @@ data = {
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
+
+
+@app.route('/login')
+def index():
+    return render_template("login.html")
+
+
+@app.route('/register')
+def index():
+    return render_template("register.html")
 
 
 @app.route("/profile", methods=["POST"])
 def login():
-    for [key]
-    form_name = request.form.get("name")
-    data["name"] = request.form.get("name")
+    for key, value in data.items():
+        data[key] = request.form.get(key)
 
-    return render_template("index.html", **context)
+    return render_template("index.html", **data)
 
 
 if __name__ == '__main__':
